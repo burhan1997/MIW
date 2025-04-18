a928eada154a99964580b1f1e0e65383c10d6edb
burhan1997 <burhanelaldi97@gmail.com>
2025-04-19 00:31:28 +0200

    introductieJava is added

---
+++ Programming/IntroductieJava/IntroductieJava/src/controller/IntroductieJavaLauncher.java
@@ -0,0 +1,12 @@
+package controller;
+public class IntroductieJavaLauncher {
+    public static void main(String[] args) {
+        String voornaam;
+        int getal;
+        voornaam = "Olivier";
+        getal = 4;
+        int kwadraat;
+        kwadraat = getal * getal;
+        System.out.println("Hallo " + voornaam + ", het kwadraat van " + getal + " is " + kwadraat);
+    }
+}

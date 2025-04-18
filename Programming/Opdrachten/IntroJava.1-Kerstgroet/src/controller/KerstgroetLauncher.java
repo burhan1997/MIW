a1181e0d0ded20d7edd8b7ef3089931caa8bfff7
burhan1997 <burhanelaldi97@gmail.com>
2025-04-18 18:50:17 +0200

    Add IntroductieJava and Kerstgroet projects to MIW

---
+++ Programming/Opdrachten/IntroJava.1-Kerstgroet/src/controller/KerstgroetLauncher.java
@@ -0,0 +1,20 @@
+/**
+ * Dit programma toont een kerstgroet.
+ * Auteur: Burhan Elaldi
+ */
+
+public class KerstgroetLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Merry Christmas!");
+        System.out.println(); // Lege regel
+
+        // De kerstboom wordt opgebouwd uit regels met *
+        System.out.println("     *");
+        System.out.println("   * * *");
+        System.out.println("    * *");
+        System.out.println("  * * * *");
+        System.out.println("   * * *");
+        System.out.println(" * * * * *");
+    }
+}

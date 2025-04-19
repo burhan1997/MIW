fef99eefabdb75cd32ac4262909814d769b93862
burhan1997 <burhanelaldi97@gmail.com>
2025-04-19 20:18:11 +0200

    more examples are added

--- Programming/MathClassEnMethods/DeMathKlasseEnMethoden/src/controller/OefeningLauncher.java
+++ Programming/MathClassEnMethods/DeMathKlasseEnMethoden/src/controller/OefeningLauncher.java
@@ -3,19 +3,28 @@ package controller;
 import java.util.Scanner;
 
 public class OefeningLauncher {
-    public static int sum(int eerstNummer, int tweedeNummer){
-        int result = 0;
-        for(int i=eerstNummer; i<=tweedeNummer; i++){
-            result = result + i;
-            System.out.println("Sum from " + eerstNummer + " to " + tweedeNummer + " is " + result);
 
-        }
-        return result;
+    public static void printHello() {
+        System.out.println("Welkom bij de Method Demo Launcher");
+        System.out.println("Dit is al de eerste methode");
     }
+
+    public static int geefRandomIntTussen1en10() {
+        int random_getal = (int)((Math.random() * 10) + 1);
+        return random_getal;
+    }
+
+    public static int geefRandomIntTussenWaardes( int onderGrens, int bovenGrens) {
+        int random_getal = (int)((Math.random() * ((bovenGrens - onderGrens) + 1)) + onderGrens);
+        return random_getal;
+    }
+
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+
         //Schrijf een programma dat vraagt om een kommagetal. Rond het gegeven getal af op twee
         //cijfers achter de komma en druk dit af.
         System.out.print("Geef een getal (bv 12,345): ");
@@ -30,19 +39,31 @@ public class OefeningLauncher {
         int willekeurigGeheelGetal = (int)(Math.random() * (10 - 2 + 1)) + 2;
         System.out.println("Willekeurig getal tussen 2 en 10: " + willekeurigGeheelGetal);
 
-        //methoden
-        int sum =0;
-        for (int i=0; i<=10; i++){
-            sum = sum + i;
-            System.out.println("Sum from 1 to 10 is " + sum);
-        }
-        for (int i=20; i<=37; i++){
-            sum += i;
-            System.out.println("Sum from 20 to 37 is " + sum);
-        }
 
-        System.out.println("Sum from 1 to 10 is " + sum(1, 10));
-        System.out.println("Sum from 20 to 37 is " + sum(20, 37));
+        System.out.println( "Demo van Math" );
+
+        System.out.println( "PI: " + Math.PI );
+
+        System.out.println( "Machtsverheffen: " + Math.pow(2,10) );
+
+        System.out.println( "Afronden: " + Math.round( 11.5 ) );
+
+        System.out.println( "Floor: " + Math.floor( 11.5 ) );
+
+        System.out.println( "Ceiling: " + Math.ceil( 11) );
+
+        System.out.println( "PI afronden op 2 cijfers: " + Math.round(Math.PI*100) / 100.0);
+        System.out.printf("PI %.2f\n", Math.PI);
+
+
+        //methoden
+        printHello();
+
+        int getal = geefRandomIntTussen1en10();
+        System.out.println("Mijn random getal is: "  + getal);
+
+        int getal2 = geefRandomIntTussenWaardes( 1, 6 );
+        System.out.println("Mijn random getal is: "  + getal2);
 
     }
 }
